1dc9729b60061e731560579fcb06003dd6a41262
Илья Морозов <pdk.04@mail.ru>
2024-12-18 22:07:57 +0300

    Preparations for hosting

--- src/main/java/com/ilyamorozov/bootpark/BootparkApplication.java
+++ src/main/java/com/ilyamorozov/bootpark/BootparkApplication.java
@@ -1,10 +1,14 @@
 package com.ilyamorozov.bootpark;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootparkApplication {
+
+	@Value("${PORT:8001}")
+	private String port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootparkApplication.class, args);
